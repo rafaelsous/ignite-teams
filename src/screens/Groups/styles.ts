1fcbd47817d5094b448b378d79a3d7890c017e24
rsousa86 <rafael.sousa@crdc.com.br>
2023-04-21 15:49:02 -0300

    Ajuste do componente Groups para usar o styled component.

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,9 +1,15 @@
-import { StyleSheet } from 'react-native'
+import styled from 'styled-components/native'
 
-export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-})
+export const Container = styled.View`
+  padding: 24px;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600 };
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD };
+  font-size: ${({ theme }) => theme.FONT_SIZE.XL}px;
+  color: ${({ theme }) => theme.COLORS.WHITE};
+`;
