b3d6779bad45ee72124124e6cf8864785c44fa52
rsousa86 <rafael.sousa@crdc.com.br>
2023-04-21 15:53:13 -0300

    Ajuste para carregamento da fonte Roboto e customização da status bar.

--- App.tsx
+++ App.tsx
@@ -1,8 +1,29 @@
-import { Groups } from '@screens/Groups';
+import { StatusBar } from 'react-native'
+import { ThemeProvider } from 'styled-components'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto'
+
+import { Groups } from '@screens/Groups'
+import { Loading } from '@components/Loading';
+
+import theme from './src/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  })
+
   return (
-    <>
-      <Groups />
-    </>
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      { fontsLoaded ? <Groups /> : <Loading /> }
+    </ThemeProvider>
   );}
