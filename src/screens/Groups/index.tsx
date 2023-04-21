0fc1d637464effdd18349ef168182164b5a657e2
rsousa86 <rafael.sousa@crdc.com.br>
2023-04-21 11:04:13 -0300

    Limpeza do projeto e implementação da primeira screen: Groups.

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+import { styles } from './styles'
+
+export function Groups() {
+  return (
+    <View style={styles.container}>
+      <Text>Groups Screen</Text>
+    </View>
+  );
+}
