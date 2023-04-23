d9a889d8eec7fdf6a8873ed75cd564df06ca6cd9
rsousa86 <rafael.sousa@crdc.com.br>
2023-04-23 15:47:07 -0300

    Implementação das screens Groups e NewGroup.

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,48 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
 import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
+import { EmptyList } from '@components/EmptyList';
+import { Button } from '@components/Button';
 
 import { Container } from './styles'
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Turma 01', 'Turma 02', 'Turma 03', 'Turma 04', 'Turma 05', 'Turma 06'])
+
   return (
     <Container>
       <Header />
+
+      <Highlight
+        title="Turmas"
+        subtitle="Jogue com sua turma"
+      />
+
+      <FlatList
+        style={{ width: '100%' }}
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            name={item}
+          />
+        )}
+        contentContainerStyle={[
+          groups.length === 0 && { flex: 1 },
+          { paddingBottom: 32 }
+        ]}
+        ListEmptyComponent={
+          <EmptyList
+            message="Que tal cadastrar a primeira turma?"
+          />
+        }
+        showsVerticalScrollIndicator={false}
+      />
+
+      <Button title="Criar nova turma" />
     </Container>
   );
 }
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components/native'
 
 export const Container = styled.View`
-  padding: 24px;
   flex: 1;
   align-items: center;
+  padding: 24px;
   background-color: ${({ theme }) => theme.COLORS.GRAY_600 };
 `;
