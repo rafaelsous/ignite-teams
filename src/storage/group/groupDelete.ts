587a2c387f54e0a46c8a0d9617e5ba9dff9f156e
rsousa86 <rafael.sousa@crdc.com.br>
2023-04-28 22:20:53 -0300

    Implementação dos serviços da entidade group.

---
+++ src/storage/group/groupDelete.ts
@@ -0,0 +1,7 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+import { GROUP_COLLECTION } from '@storage/storageConfig'
+
+export async function groupDelete(groupName: string) {
+  
+}
