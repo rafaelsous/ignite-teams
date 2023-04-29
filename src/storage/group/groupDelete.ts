9f88965d76d969f6b4832a348998f4119357d93f
rsousa86 <rafael.sousa@crdc.com.br>
2023-04-29 14:20:30 -0300

    Implementação do serviços de exclusão de group por nome e de players por group.

--- src/storage/group/groupDelete.ts
+++
@@ -1,7 +0,0 @@
-import AsyncStorage from '@react-native-async-storage/async-storage'
-
-import { GROUP_COLLECTION } from '@storage/storageConfig'
-
-export async function groupDelete(groupName: string) {
-  
-}
