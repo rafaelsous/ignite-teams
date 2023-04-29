ac57f84388e82f04d172bffcea1ce185473d9531
rsousa86 <rafael.sousa@crdc.com.br>
2023-04-28 22:21:05 -0300

    Implementação dos serviços da entidade player.

---
+++ src/storage/player/playerAddByGroup.ts
@@ -0,0 +1,26 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+import { PLAYER_COLLECTION } from '@storage/storageConfig'
+import { AppError } from '@utils/AppError'
+
+import { PlayerStorageDTO } from './PlayerStorageDTO'
+import { playersGetByGroup } from './playersGetByGroup'
+
+export async function playerAddByGroup(newPlayer: PlayerStorageDTO, group: string) {
+  try {
+    const storedPlayers = await playersGetByGroup(group)
+    const playerAlreadyExists = storedPlayers.filter(player => player.name === newPlayer.name)
+
+    if (playerAlreadyExists.length > 0) {
+      throw new AppError('Esse jogador já foi adicionado a um time.')
+    }
+
+    const storage = JSON.stringify([...storedPlayers, newPlayer])
+
+    await AsyncStorage.setItem(`${PLAYER_COLLECTION}-${group}`, storage)
+  } catch (error) {
+    throw error
+  }
+
+
+}
